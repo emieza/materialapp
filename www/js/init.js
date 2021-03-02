5bb938dd69628a93504b585e85b31af95d092d6d
Enric <emieza@xtec.cat>
2021-03-02 18:03:05 +0100

    primer

---
+++ www/js/init.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+
+  	// init GUI
+    $('.sidenav').sidenav();
+	$('.tabs').tabs({"swipeable":true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    
+
+
+}
