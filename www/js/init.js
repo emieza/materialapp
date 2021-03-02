140783dc24169b9e576891dfd92beb0133593075
Enric <emieza@xtec.cat>
2021-03-02 20:00:20 +0100

    musicbrainz search

--- www/js/init.js
+++ www/js/init.js
@@ -9,6 +9,39 @@
 	// els tabs necessiten ser swipeable
 	$('.tabs').tabs({"swipeable":true});
 
+
+	// botó cerca banda
+	$('#searchButton').click(function() {
+
+		var text = $("#searchText").val();
+
+		$.ajax({
+			method: "GET",
+  			url: "https://musicbrainz.org/ws/2/artist?query="+text,
+			dataType: "json",
+		}).done(function (msg) {
+			// buidem llista
+			$(".collection").empty();
+			// omplim amb les dades de la API
+			for(var i in msg.artists) {
+				var artist = msg.artists[i];
+				var elem = $("\
+					<li class='collection-item avatar'>\
+						<img src='img/user.jpg' class='circle'>\
+						<span class='title'>"+artist.name+"</span>\
+						<p>"+artist.disambiguation+"</p>\
+						<a href='#!' class='secondary-content'>\
+							<i class='material-icons'>text_snippet</i>\
+						</a>\
+					</li>");
+				$(".collection").append(elem);
+				//console.log(msg.artists[i]);
+			};
+		}).fail(function () {
+			alert("ERROR");
+		});
+	});
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
