258c7a2a9c065b1a8d88614d9b41a2718c316d86
Enric <emieza@xtec.cat>
2021-03-02 19:54:25 +0100

    floating action button

--- www/js/init.js
+++ www/js/init.js
@@ -1,28 +1,20 @@
 (function($){
   $(function(){
 
+  	// on document ready...
+
   	// init GUI
     $('.sidenav').sidenav();
+	$('.fixed-action-btn').floatingActionButton();
+	// els tabs necessiten ser swipeable
 	$('.tabs').tabs({"swipeable":true});
-
-	// botó cerca banda
-	$('#searchButton').click(function() {
-		alert("buscant...");
-
-	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 
-
 document.addEventListener('deviceready', onDeviceReady, false);
-
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    
-
-
 }
