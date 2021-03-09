4606bb58aa761781083690039fad2ac8416001ee
Enric <emieza@xtec.cat>
2021-03-09 16:31:52 +0100

    link tab

--- www/js/init.js
+++ www/js/init.js
@@ -5,9 +5,19 @@
 
   	// init GUI
     $('.sidenav').sidenav();
-	$('.fixed-action-btn').floatingActionButton();
-	// els tabs necessiten ser swipeable
-	$('.tabs').tabs({"swipeable":true});
+	  $('.fixed-action-btn').floatingActionButton();
+	  // els tabs necessiten ser swipeable
+	  //$('.tabs').tabs({"swipeable":true});
+
+    var options = { "swipeable": true };
+    var el = document.getElementById('tabs-swipe-demo');
+    var tabsInstance = M.Tabs.init(el, options);
+
+    // make clicable an element of the list and go to the next tab
+
+    $('.collection-item.active').click(function() {
+        tabsInstance.select("test-swipe-2");
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
