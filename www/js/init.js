7e8f064277e07f35b102bd8992a9a009fd9d4e22
Enric Mieza <emieza@xtec.cat>
2023-02-04 01:00:57 +0100

    captura d'imatge i visualització

--- www/js/init.js
+++ www/js/init.js
@@ -19,12 +19,55 @@
         tabsInstance.select("test-swipe-2");
     });
 
+    $('#takePicture').click(takePicture);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 
+var deviceReady = false;
 document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    deviceReady = true;
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 }
+
+
+function takePicture() {
+  if(!deviceReady) {
+    alert("La hardware per a la càmera no s'ha inicialitzat.");
+    return;
+  }
+
+  var cameraOptions = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        encodingType : Camera.EncodingType.JPEG,
+        correctOrientation: true,
+        sourceType: Camera.PictureSourceType.CAMERA
+  }
+
+  navigator.camera.getPicture(
+    function(imageURI) {
+    resolveLocalFileSystemURL(imageURI, function(fileEntry) {
+      // fileEntry is usable for uploading without holding image in memory...
+      
+      fileEntry.file(function(file) { 
+        var reader = new FileReader();
+    
+        reader.onloadend = function() {
+          // this.result contains the Data URI usable as a preview thumbnail
+          $('#myImage').attr('src', this.result);
+        }
+    
+        reader.readAsDataURL(file);
+      }, cameraError);
+    }, cameraError);
+  }, cameraError, cameraOptions);
+
+}
+
+
+function cameraError(err) {
+  alert("ERROR: "+err);
+}
+
